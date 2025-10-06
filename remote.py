a3d833123397cef932eb3bb31cc6b2941ea8cbbd
Edris-telegram <imadechristabel@gmail.com>
2025-10-06 16:13:14 +0100

    Create remote.py

---
+++ remote.py
@@ -0,0 +1,14 @@
+import asyncio
+from playwright.async_api import async_playwright
+
+BROWSERLESS_WS = "wss://chrome.browserless.io?token=2TBcCoG2vzP3UFs93f92e1471c723563c170e676ec2e2d255"
+
+async def run():
+    async with async_playwright() as p:
+        browser = await p.chromium.connect_over_cdp(BROWSERLESS_WS)
+        page = await browser.new_page()
+        await page.goto("https://www.google.com")
+        print("✅ Page title:", await page.title())
+        await browser.close()
+
+asyncio.run(run())
